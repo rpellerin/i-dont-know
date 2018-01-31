c138bda385284a16d8f81961ec4d110a12074780
Romain Pellerin <contact@romainpellerin.eu>
2018-02-01 00:07:34 +0100

    Add missing className

--- README.md
+++ README.md
@@ -6,9 +6,10 @@
   * cd build
   * git init
   * git remote add
+  * git fetch
   * git add -A
-  * git commit
-  * git checkout -b gh-pages origin/gh-pages
+  * git commit --no-verify -m 'Release'
+  * git checkout -b gh-pages
   * git cherry-pick master --strategy-option theirs
   * git push
 * Cool font + Variable fonts: https://medium.com/clear-left-thinking/how-to-use-variable-fonts-in-the-real-world-e6d73065a604 & https://css-tricks.com/one-file-many-options-using-variable-fonts-web/
--- src/components/video.js
+++ src/components/video.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 export default ({ url, tabIndex }) => (
-  <div className="aspect-ratio aspect-ratio--1x1 bg-black" tabIndex={tabIndex}>
+  <div
+    className="tile aspect-ratio aspect-ratio--1x1 bg-black"
+    tabIndex={tabIndex}
+  >
     <iframe
       title={url}
-      className="tile aspect-ratio--object"
+      className="aspect-ratio--object"
       src={`https://www.youtube-nocookie.com/embed/${url}?rel=0&amp;showinfo=0`}
       frameBorder="0"
       allowFullScreen
