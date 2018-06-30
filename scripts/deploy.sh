3579e2bd6793664fa812f7da3355f5669f457408
Romain Pellerin <contact@romainpellerin.eu>
2018-06-30 15:04:56 +0200

    Improve deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,11 @@ git init
 git remote add origin https://${GH_TOKEN}@github.com/rpellerin/i-dont-know.git
 git fetch origin
 git add -A
+git commit -m 'Latest build' -n
+git diff-index origin/gh-pages --binary -p > ../diff
+git checkout -b gh-pages origin/gh-pages
+git apply ../diff
+git add -A
 date=$(date --iso-8601=seconds)
 git commit --no-verify -m "Release-$date"
-git checkout -b gh-pages origin/gh-pages
-git cherry-pick master --strategy-option theirs
 git push -q origin gh-pages:gh-pages >/dev/null 2>&1
