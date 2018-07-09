4d091109732925ea72f415167a2c1754c57d6f8d
Romain Pellerin <contact@romainpellerin.eu>
2018-07-09 22:50:53 +0200

    Add links

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
+
+TARGET_BRANCH="gh-pages"
+DATE=$(date --iso-8601=seconds)
+
 cd build
 git init
 git remote add origin https://${GH_TOKEN}@github.com/rpellerin/i-dont-know.git
 git fetch origin
+
+BRANCH_EXISTS=`git rev-parse --verify origin/$TARGET_BRANCH`
+
+if [ -n "$BRANCH_EXISTS" ]; then
+    echo "Branch $TARGET_BRANCH exists"
+    git add -A
+    git commit --no-verify -m 'Latest build'
+    rm ../diff -f
+    git diff-index origin/$TARGET_BRANCH --binary -p > ../diff
+    if [ ! -s ../diff ]; then
+        echo "$TARGET_BRANCH has not changed. Won't push.";
+        exit 0
+    fi
+    git checkout -b $TARGET_BRANCH origin/$TARGET_BRANCH
+    git apply ../diff
+else
+    echo "Branch $TARGET_BRANCH does not exist"
+    git checkout -b $TARGET_BRANCH
+fi
+
 git add -A
-git commit -m 'Latest build' -n
-git diff-index origin/gh-pages --binary -p > ../diff
-git checkout -b gh-pages origin/gh-pages
-git apply ../diff
-git add -A
-date=$(date --iso-8601=seconds)
-git commit --no-verify -m "Release-$date"
-git push -q origin gh-pages:gh-pages >/dev/null 2>&1
+git commit --no-verify -m "Release-$DATE"
+git push -q origin $TARGET_BRANCH:$TARGET_BRANCH >/dev/null 2>&1
