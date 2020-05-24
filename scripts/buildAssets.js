db31d258ac75e9d2aa8c1fd0f05de629f95b0ada
Romain Pellerin <rpellerin@users.noreply.github.com>
2020-05-24 15:35:32 +0200

    Improve buildAssets

--- scripts/buildAssets.js
+++ scripts/buildAssets.js
@@ -13,22 +13,25 @@ const TEXTS_FILE = path.join(__dirname, '..', 'public', 'static', 'texts.txt')
 const OUTPUT_FILE = path.join(__dirname, '..', 'src', 'assets.json')
 
 // https://stackoverflow.com/questions/1484506/random-color-generator
-const getRandomColor = () => {
-  const num = Math.round(0xffffff * Math.random())
-  const r = num >> 16
-  const g = (num >> 8) & 255
-  const b = num & 255
-  return `rgb(${r},${g},${b})`
-}
+const getRandomColor = (hash) =>
+  `#${Math.abs(hash)
+    .toString(16)
+    .split('')
+    .reverse()
+    .join('')
+    .slice(0, 6)}`
 
 const hashCode = (s) =>
   s.split('').reduce((a, b) => ((a << 5) - a + b.charCodeAt(0)) | 0, 0)
 
-const sortArray = (array) =>
+const buildAssets = (array) =>
   array
     .map((item) => ({ item, hash: hashCode(JSON.stringify(item)) }))
     .sort((a, b) => a.hash - b.hash)
-    .map(({ item }) => item)
+    .map(({ item, hash }) => ({
+      ...item,
+      backgroundColor: getRandomColor(hash)
+    }))
 
 const images = fs
   .readdirSync(PICTURES_DIRECTORY)
@@ -49,11 +52,11 @@ const texts = fs
     return { text: match[1].trim(), href: match[2].trim() }
   })
 
-const assets = sortArray([
+const assets = buildAssets([
   ...images.map((path) => ({ path, type: 'IMAGE' })),
   ...videos.map((url) => ({ url, type: 'VIDEO' })),
   ...texts.map(({ text, href }) => ({ text, href, type: 'TEXT' }))
-]).map((asset) => ({ backgroundColor: getRandomColor(), ...asset }))
+])
 
 const outputFileContent = JSON.stringify(assets, null, '\t')
 
